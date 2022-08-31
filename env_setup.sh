87d62be5166a0fafca630cb8cd43773644de24c0
Arturs Elksnis <archie1983@hotmail.co.uk>
2022-08-31 13:38:25 +0100

    Adapting ROS TCP endpoint system variables

--- env_setup.sh
+++ env_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export ROS_TCP_ENDPOINT_IPADDR="192.168.8.199"
+export ROS_TCP_ENDPOINT_IPADDR="192.168.8.113"
 export ROS_TCP_ENDPOINT_PORT="10000"
 #source /home/jet/robotics/cartographer_ws/devel_isolated/setup.bash
 source /home/pi/robotics/dynamixel_pan_tilt_ros/devel/setup.bash
